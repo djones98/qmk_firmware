ef60aa50bb025165a384d9fdd7b6d31e60e8a121
djones98 <jonesdarren1245@gmail.com>
2023-01-06 19:16:28 +0000

    add fancyalice66 support

--- keyboards/djones98/libramini/keymaps/via/keymap.c
+++ keyboards/djones98/libramini/keymaps/via/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_QUOT,
 		KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
 		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, TG(1), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
-		KC_RCTL, KC_LGUI, KC_LALT, LT(1,KC_SPACE), LT(2,KC_BSPC), ARROW_KEYS, KC_BTN1, KC_BTN2, KC_RSFT,KC_DEL),
+		KC_LCTL, KC_LGUI, KC_LALT, LT(1,KC_SPACE), LT(2,KC_BSPC), ARROW_KEYS, KC_BTN1, KC_BTN2, KC_RSFT,KC_DEL),
 
 	LAYOUT(
 		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
@@ -126,10 +126,3 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
     return mouse_report;
 }
 
-// void keyboard_post_init_user(void) {
-//     // Customise these values to desired behaviour
-//     debug_enable   = true;
-//     debug_matrix   = false;
-//     debug_keyboard = false;
-//     debug_mouse    = false;
-// }
